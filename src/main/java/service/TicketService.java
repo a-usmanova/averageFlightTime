fd72400f16fba8b861d672bb8c3b48aeaf3c9086
Alena <126641708+a-usmanova@users.noreply.github.com>
2023-03-13 18:50:24 +1000

    answer format fixed

--- src/main/java/service/TicketService.java
+++ src/main/java/service/TicketService.java
@@ -8,9 +8,8 @@ import model.TicketWrapper;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 @Data
 public class TicketService {
@@ -21,19 +20,21 @@ public class TicketService {
         fullTimeOfFlights();
     }
 
-    public double getAverageFlightTime() {
-        if (timeOfFlights.isEmpty()) return 0;
-        return timeOfFlights.stream()
+    public String getAverageFlightTime() {
+        if (timeOfFlights.isEmpty()) return "";
+        int averageTime = (int) timeOfFlights.stream()
                 .mapToLong(a -> a)
                 .average().orElse(0);
+        return new SimpleDateFormat("hh:mm").format(new Date(averageTime * 1000L));
+
     }
 
-    public double getPercentile(int percentile) {
-        if (timeOfFlights.isEmpty()) return 0;
+    public String getPercentile(int percentile) {
+        if (timeOfFlights.isEmpty()) return "";
         Collections.sort(timeOfFlights);
         int index = (int) Math.ceil(((double) percentile / 100) * timeOfFlights.size());
 
-        return timeOfFlights.get(index - 1);
+        return new SimpleDateFormat("hh:mm").format(new Date(timeOfFlights.get(index - 1) * 1000));
     }
 
     private void fullTimeOfFlights() {
@@ -43,7 +44,6 @@ public class TicketService {
         try {
             TicketWrapper wrapper = objectMapper.readValue(jsonString, TicketWrapper.class);
             List<Ticket> tickets = wrapper.getTickets();
-
             tickets.forEach(t -> timeOfFlights.add(t.getArrivalDate().toEpochSecond() - t.getDepartureDate().toEpochSecond()));
 
         } catch (JsonProcessingException e) {
